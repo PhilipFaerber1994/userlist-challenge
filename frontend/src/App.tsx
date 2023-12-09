a5eb3c9cfed6380410e2e2114bc5a2af5ed56bec
PhilipFaerber1994 <philipfaerber@yahoo.com>
2023-12-09 12:00:48 +0100

    created navbar and styled it (30min)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import UserListPage from "./pages/UserListPage";
 import { CreateUserPage } from "./pages/CreateUserPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<UserListPage />} />
           <Route path="/create-user" element={<CreateUserPage />} />
         </Routes>
+        <Outlet />
       </BrowserRouter>
     </div>
   );
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+const Navbar = () => {
+  const [activeLink, setActiveLink] = useState<Number>(0);
+  const links = [
+    {
+      to: "/",
+      linkTitle: "Benutzerliste",
+    },
+    {
+      to: "/create-user",
+      linkTitle: "Benutzer anlegen",
+    },
+  ];
+
+  const activateLink = (index: Number) => {
+    setActiveLink(index);
+  };
+
+  return (
+    <nav className="p-10 flex justify-end">
+      {links.map((link, index) => (
+        <Link key={index} to={link.to} onClick={() => activateLink(index)}>
+          <span
+            className={`${
+              activeLink === index
+                ? "bg-emerald-500 text-white font-bold rounded"
+                : ""
+            } p-4`}
+          >
+            {link.linkTitle}
+          </span>
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Navbar;
