fe25bfd8b89e6472c6e05c80faa95cfb28fa1e10
PhilipFaerber1994 <philipfaerber@yahoo.com>
2023-12-09 15:03:45 +0100

    changed div based layout to table and added  conditional rendering for editting (20min)

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IUser } from "../UserInterface";
 import Button from "./Button";
 
@@ -8,43 +8,89 @@ interface IModal {
 }
 
 const Modal = ({ user, closeModal }: IModal) => {
+  const [edit, setEdit] = useState<boolean>(false);
+
+  const changeToEditMode = () => {
+    setEdit(true);
+  };
+
   return (
     <>
       <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center">
-        <div className="flex flex-col items-center bg-white p-8 rounded shadow-md w-1/4 h-1/2">
-          {/* I wrapped the four p-tags inside two div-tags to make the edit modus look better */}
-          <div className="flex flex-row justify-left w-full">
-            <p className="mr-5">{user.firstname}</p>
-            <p>{user.lastname}</p>
-          </div>
-          <div className="flex flex-row justify-left w-full">
-            <p className="mr-5">{String(user.age)}</p>
-            <p>{user.eMail}</p>
-          </div>
-          <div className="flex flex-row justify-around mt-auto">
-            {/* Use mt-auto to push the button div to the bottom */}
-            <div>
+        <div className="flex flex-col items-center bg-white p-8 rounded shadow-md w-1/2 h-1/4">
+          {edit ? (
+            <table>
+              <tbody>
+                <tr>
+                  <td className="text-left pr-5">
+                    <input
+                      className="bg-gray-300 p-2 rounded"
+                      type="text"
+                      value={`${user.firstname}`}
+                    />
+                  </td>
+                  <td className="text-left pr-5">
+                    <input
+                      className="bg-gray-300 p-2 rounded"
+                      type="text"
+                      value={`${user.lastname}`}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-left pr-5">
+                    <input
+                      className="bg-gray-300 p-2 rounded"
+                      type="text"
+                      value={`${user.age.toString()}`}
+                    />
+                  </td>
+                  <td className="text-left pr-5">
+                    <input
+                      className="bg-gray-300 p-2 rounded"
+                      type="text"
+                      value={`${user.eMail}`}
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          ) : (
+            <table>
+              <tbody>
+                <tr>
+                  <td className="text-left pr-5">{user.firstname}</td>
+                  <td className="text-left pr-5">{user.lastname}</td>
+                </tr>
+                <tr>
+                  <td className="text-left pr-5">{String(user.age)}</td>
+                  <td className="text-left pr-5">{user.eMail}</td>
+                </tr>
+              </tbody>
+            </table>
+          )}
+          <div className="flex justify-around mt-auto">
+            <Button
+              color="bg-red-500"
+              hoverColor="bg-red-600"
+              title="löschen"
+              clickFunction={() => closeModal()}
+            />
+            <Button
+              color="bg-amber-500"
+              hoverColor="bg-amber-600"
+              title="bearbeiten"
+              clickFunction={() => changeToEditMode()}
+            />
+            <div className="flex justify-around mt-auto">
               <Button
-                color="bg-red-500"
-                hoverColor="bg-red-600"
-                title="löschen"
-                clickFunction={() => closeModal()}
-              />
-              <Button
-                color="bg-amber-500"
-                hoverColor="bg-amber-600"
-                title="bearbeiten"
+                color="bg-gray-500"
+                hoverColor="bg-gray-600"
+                title="schließen"
                 clickFunction={() => closeModal()}
               />
             </div>
-            <Button
-              color="bg-gray-500"
-              hoverColor="bg-gray-600"
-              title="schließen"
-              clickFunction={() => closeModal()}
-            />
           </div>
-          <div className="flex flex-row justify-around mt-auto"></div>
         </div>
       </div>
     </>
