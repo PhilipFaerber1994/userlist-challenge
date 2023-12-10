8f80b2128b4d123252b94ee77fa08bf2d2f3ba48
PhilipFaerber1994 <philipfaerber@yahoo.com>
2023-12-10 12:47:30 +0100

    implemented formik and validation (25min)

--- frontend/src/components/UserForm.tsx
+++ frontend/src/components/UserForm.tsx
@@ -4,6 +4,7 @@ import Button from "./Button";
 import axios from "axios";
 import { API_ENUMS } from "../API_ENUMS";
 import { validationSchema } from "../validationSchema";
+import { useFormik } from "formik";
 
 const UserForm = () => {
   const [editUser, setEditUser] = useState<IUser>({
@@ -13,6 +14,18 @@ const UserForm = () => {
     age: 0,
     eMail: "",
   });
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: {
+        // _id: "",
+        firstname: "",
+        lastname: "",
+        age: 0,
+        eMail: "",
+      },
+      validationSchema: validationSchema,
+      onSubmit: () => createUser(),
+    });
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -24,9 +37,9 @@ const UserForm = () => {
 
   const createUser = async () => {
     try {
-      const { _id, ...userData } = editUser;
-      await axios.post(API_ENUMS.BASE_URL + "/user/", userData).then((res) => {
-        console.log("Nutzer angelegt: ", editUser);
+      //   const { _id, ...values } = editUser;
+      await axios.post(API_ENUMS.BASE_URL + "/user/", values).then((res) => {
+        console.log("Nutzer angelegt: ", values);
       });
     } catch (error) {
       throw error;
@@ -38,51 +51,53 @@ const UserForm = () => {
       <label className="text-left text-emerald-500" htmlFor="firstname">
         Vorname
       </label>
-      <input
-        className="bg-gray-300 p-2 rounded mb-5"
-        id="firstname"
-        type="text"
-        value={`${editUser.firstname}`}
-        name="firstname"
-        onChange={(e) => handleInputChange(e)}
-      />
-      <label className="text-left text-emerald-500" htmlFor="lastname">
-        Nachname
-      </label>
-      <input
-        className="bg-gray-300 p-2 rounded mb-5"
-        type="text"
-        value={`${editUser.lastname}`}
-        name="lastname"
-        onChange={(e) => handleInputChange(e)}
-      />
-      <label className="text-left text-emerald-500" htmlFor="firstname">
-        Alter
-      </label>
-      <input
-        className="bg-gray-300 p-2 rounded mb-5"
-        type="text"
-        value={`${editUser.age}`}
-        name="age"
-        onChange={(e) => handleInputChange(e)}
-      />
-      <label className="text-left text-emerald-500" htmlFor="firstname">
-        E-Mail
-      </label>
-      <input
-        className="bg-gray-300 p-2 rounded mb-5"
-        type="text"
-        value={`${editUser.eMail}`}
-        name="eMail"
-        onChange={(e) => handleInputChange(e)}
-      />
+      <form className="flex flex-col" onSubmit={handleSubmit}>
+        <input
+          className="bg-gray-300 p-2 rounded mb-5"
+          id="firstname"
+          type="text"
+          value={values.firstname}
+          name="firstname"
+          onChange={handleChange}
+        />
+        <label className="text-left text-emerald-500" htmlFor="lastname">
+          Nachname
+        </label>
+        <input
+          className="bg-gray-300 p-2 rounded mb-5"
+          type="text"
+          value={values.lastname}
+          name="lastname"
+          onChange={handleChange}
+        />
+        <label className="text-left text-emerald-500" htmlFor="firstname">
+          Alter
+        </label>
+        <input
+          className="bg-gray-300 p-2 rounded mb-5"
+          type="text"
+          value={values.age}
+          name="age"
+          onChange={handleChange}
+        />
+        <label className="text-left text-emerald-500" htmlFor="firstname">
+          E-Mail
+        </label>
+        <input
+          className="bg-gray-300 p-2 rounded mb-5"
+          type="text"
+          value={values.eMail}
+          name="eMail"
+          onChange={handleChange}
+        />
 
-      <Button
-        color="bg-emerald-500"
-        hoverColor="bg-emerald-600"
-        title="speichern"
-        clickFunction={() => createUser()}
-      />
+        <Button
+          color="bg-emerald-500"
+          hoverColor="bg-emerald-600"
+          title="speichern"
+          clickFunction={handleSubmit}
+        />
+      </form>
     </div>
   );
 };
